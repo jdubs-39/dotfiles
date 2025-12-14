33d4d177aee96cadec31d1a3f9dc5f5600b26fc3
jdubs-39 <junweilee72@gmail.com>
2025-12-15 05:47:06 +0800

    switched vim.o.termguicolors to false to use terminal colors

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 require("config.lazy")
 vim.wo.relativenumber = true
 vim.diagnostic.config({ virtual_text = true, underline = false, update_in_insert = true })
-vim.opt.termguicolors = true
+vim.opt.termguicolors = false
 vim.opt.signcolumn = "yes"
