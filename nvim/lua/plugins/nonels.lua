e76cf2b002fd48899e50eeb5d9e6a37b9a702cc9
jdubs-39 <junweilee72@gmail.com>
2025-12-17 23:39:40 +0800

    added isort as a formatter for python imports

--- nvim/lua/plugins/nonels.lua
+++ nvim/lua/plugins/nonels.lua
@@ -6,6 +6,7 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
 				null_ls.builtins.formatting.stylua,
 			},
 			on_attach = function(client, bufnr)
