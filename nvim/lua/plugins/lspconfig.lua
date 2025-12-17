1c9416436c817df468483f99cc8cd73c5958f01b
jdubs-39 <junweilee72@gmail.com>
2025-12-17 21:22:21 +0800

    adding back basedpyright as an lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -5,7 +5,7 @@ return {
 	},
 	{
 		"mason-org/mason-lspconfig.nvim",
-		opts = { ensure_installed = { "ruff", "lua_ls", "zls" } },
+		opts = { ensure_installed = { "ruff", "basedpyright", "lua_ls", "zls" } },
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
 			"neovim/nvim-lspconfig",
@@ -19,6 +19,7 @@ return {
 		config = function()
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
 			vim.lsp.enable("ruff", { capabilities = capabilities })
+			vim.lsp.enable("basedpyright", { capabilities = capabilities })
 			vim.lsp.enable("lua_ls", { capabilities = capabilities })
 			vim.lsp.enable("zls", { capabilities = capabilities })
 		end,
