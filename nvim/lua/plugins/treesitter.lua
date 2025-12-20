8d46247b0fb69a82418082bab0fd1035733b2bf7
jdubs-39 <junweilee72@gmail.com>
2025-12-20 16:49:52 +0800

    updating treesitter's ensure installed list

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local tsconfig = require("nvim-treesitter.configs")
 		tsconfig.setup({
-			ensure_installed = { "lua", "python", "zig" },
+			ensure_installed = { "lua", "python", "ocaml", "ocaml_interface" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
